8a16cbabe1622bfec57bc40e2300807b435486d7
sanyamjain4321 <ks2578@srmist.edu.in>
2026-02-25 14:34:46 +0530

    usecase1 for palindrome checker

---
+++ src/Palindrome_Checker.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Palindrome_Checker {
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome checker management System");
+        System.out.println("Version:1.0");
+        System.out.println("System initialised successfully");
+    }
+}

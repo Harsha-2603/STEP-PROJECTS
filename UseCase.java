1d17282c89ddcfb4b8d77c10586785653a0b3576
Harsha vardhan <harshavardhan.thatavarthi@gmail.com>
2026-02-28 09:23:38 +0530

    usecase3

--- UseCase.java
+++ UseCase.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
 public class UseCase {
     public static void main(String[] args) {
-
-        String word = "madam";
+        // Take input from user
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String original = sc.nextLine();
         String reversed = "";
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
+        // Reverse the string
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
-
-        if (word.equals(reversed)) {
-            System.out.println(word + " is a palindrome");
+        // Check palindrome
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("Palindrome");
         } else {
-            System.out.println(word + " is not a palindrome");
+            System.out.println("Not Palindrome");
         }
+        sc.close(); // close scanner
     }
 }
